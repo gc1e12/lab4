00e9c12ef588ec75d55f5eb172ffe22cc92c69b0
gc1e12 <AARON-MAC@ug-vpn15.ecs.soton.ac.uk>
2013-10-24 14:44:42 +0100

    Index2 : Edit car.class.php
    Update attributes and constructor

--- cars.php
+++ cars.php
@@ -1,6 +1,7 @@
 <?php include('includes/header.php'); ?>
 <?php include('data/carData.php'); ?>
 
+
 <!-- header -->
 <div id="header">
 	<!-- title -->
---
+++ classes/car.class.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+This class create a car object.
+*/
+
+
+    class Car{
+    
+            private $make;
+            private $model;
+            private $description;
+            private $price;
+            private $regNumber;
+            private $regYear;
+            
+            
+            
+            public function __construct ($make , $model , $description , $price, $regNumber, $regYear){
+                
+                $this->make = $name;
+                $this->model = $model;
+                $this->description = $description;
+                $this->price = $price;
+                $this->refNumber = $regNumber;
+                $this->regYear = $regYear;                
+            }
+        
+    
+    
+    
+    }
+
+?>
