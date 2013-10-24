5fc43d05777c3e2e473665c101aadc0380141639
gc1e12 <AARON-MAC@ug-vpn15.ecs.soton.ac.uk>
2013-10-24 16:09:50 +0100

    Index 4 : adding comment to each method in class

--- classes/car.class.php
+++ classes/car.class.php
@@ -15,7 +15,11 @@ This class create a car object.
             private $regYear;//regYear? manufacture year?
             
             
-            
+            /*
+             *This is a contsructor to create a new car
+             *@param make,model,description,price,regNumber,regyear
+             *@return
+             */
             public function __construct ($make , $model , $description , $price, $regNumber, $regYear){
                 
                 $this->make = $make;
@@ -26,30 +30,66 @@ This class create a car object.
                 $this->regYear = $regYear;                
             }
             
+            /*
+             *This is getter method for getting the make
+             *@param
+             *@return $this->make
+             */
             public function getMake(){
                 return $this->make;
             }
             
+            
+            /*
+             *This is getter method for getting the model
+             *@param 
+             *@return $this->model
+             */
             public function getModel(){
                 return $this->model;
             }
             
+            /*
+             *This is getter method for getting the description of the car
+             *@param 
+             *@return $this->description
+             */
             public function getDescription(){
                 return $this->description;
             }
             
+            /*
+             *This is getter method for getting the price of the car
+             *@param 
+             *@return $this->price
+             */
             public function getPrice(){
                 return $this->price;
             }
             
+            /*
+             *This is getter method for getting the regNumber
+             *@param 
+             *@return $this->regNumber
+             */
             public function getRegNumber(){
                 return $this->regNumber;
             }
             
+            /*
+             *This is getter method for getting the regYear
+             *@param 
+             *@return $this->regYear
+             */
             public function getRegYear(){
                 return $this->regYear;
             }
             
+            /*
+             *This is getter method to calculate the car's age
+             *@param 
+             *@return car age
+             */
             public function getCarAge(){
                 
                 $todayYear = date("Y");
